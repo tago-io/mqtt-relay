c83f293df17b2bc6f52e960c485ce405d62f7741
Vitor Lima <vitor@ferreiradelima.com>
2024-06-18 19:25:12 -0300

    chore: test dockerhub build

--- dockerhub.sh
+++ dockerhub.sh
@@ -41,9 +41,9 @@ CARGO_SERVER_SSL_KEY_BASE64=$(echo "$CARGO_SERVER_SSL_KEY" | base64 -w 0)
 # Alpine
 # docker buildx build --push --build-arg TAGORELAY_VERSION=${FULL_VERSION} \
 #   --platform linux/arm64/v8,linux/amd64 \
-#   --tag tagoio/tagorelay:alpine \
-#   --tag tagoio/tagorelay:${MAJOR}.${MINOR}-alpine \
-#   --tag tagoio/tagorelay:${MAJOR}.${MINOR}.${PATCH}-alpine .
+#   --tag tagoio/relay:alpine \
+#   --tag tagoio/relay:${MAJOR}.${MINOR}-alpine \
+#   --tag tagoio/relay:${MAJOR}.${MINOR}.${PATCH}-alpine .
 
 # Debian
 docker buildx build --push --build-arg TAGORELAY_VERSION=${FULL_VERSION} \
@@ -51,11 +51,11 @@ docker buildx build --push --build-arg TAGORELAY_VERSION=${FULL_VERSION} \
   --build-arg CARGO_SERVER_SSL_CERT=${CARGO_SERVER_SSL_CERT_BASE64} \
   --build-arg CARGO_SERVER_SSL_KEY=${CARGO_SERVER_SSL_KEY_BASE64} \
   --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
-  --tag tagoio/tagorelay \
-  --tag tagoio/tagorelay:debian \
-  --tag tagoio/tagorelay:bullseye \
-  --tag tagoio/tagorelay:${MAJOR}.${MINOR}-bullseye \
-  --tag tagoio/tagorelay:${MAJOR}.${MINOR}.${PATCH}-bullseye \
-  --tag tagoio/tagorelay:${MAJOR}.${MINOR} \
-  --tag tagoio/tagorelay:${MAJOR}.${MINOR}.${PATCH} \
+  --tag tagoio/relay \
+  --tag tagoio/relay:debian \
+  --tag tagoio/relay:bullseye \
+  --tag tagoio/relay:${MAJOR}.${MINOR}-bullseye \
+  --tag tagoio/relay:${MAJOR}.${MINOR}.${PATCH}-bullseye \
+  --tag tagoio/relay:${MAJOR}.${MINOR} \
+  --tag tagoio/relay:${MAJOR}.${MINOR}.${PATCH} \
   .
